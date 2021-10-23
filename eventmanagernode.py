d29665fb5a6070f6b401406b6cf2f0fa2bec4d8b
Shri Vignesh Senthil Kumar <shri0101silence@gmail.com>
2021-10-23 19:07:59 -0400

    Add files via upload

---
+++ eventmanagernode.py
@@ -0,0 +1,26 @@
+import zmq
+import json
+
+context = zmq.Context()
+socket = context.socket(zmq.PULL)
+socket.bind("tcp://*:5555")
+poller = zmq.Poller()
+poller.register(socket, zmq.POLLIN)
+
+def publish(message):
+    socket1 = context.socket(zmq.PUSH)
+    socket1.connect("tcp://localhost:6000")
+    socket1.send_json(json.dumps(message))
+    socket1.close()    
+    return 
+
+print('Looking out for messages!')
+while True:
+    socks = dict(poller.poll())
+    if socket in socks:
+         message = socket.recv_json()
+         print(type(message))
+         print("Received below payload from "+"!!!!")
+         publish(message)
+         
+
